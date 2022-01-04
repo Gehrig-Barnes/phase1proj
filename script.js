a8208e99f14a320534a3c6f4d5d49aeedc17d825
RodneyHill <rlhill888@gmail.com>
2022-01-04 12:40:41 -0500

    crrated click events

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 function resturantData () {
     fetch("http://localhost:3000/resturants")
     .then (res => res.json())
-    .then (data =>{ data.forEach(renderSteaks)})
+    .then (data =>{ 
+        data.forEach(renderSteaks)
+    })
 }
 
-function renderSteaks (steaks) {
+function renderSteaks (obj) {
     const resturants = document.querySelector("#logo")
     const logoImage = document.createElement('img')
-    logoImage.setAttribute('src', `${steaks.logo}`)
+    logoImage.setAttribute('src', `${obj.logo}`)
     resturants.appendChild(logoImage)
+    logoImage.addEventListener('click', ()=>{
+        totalLikes(obj)
+        const name =document.querySelector('.name')
+        const img =document.querySelector('.detailimg')
+        const address = document.querySelector('.address')
+        const phoneNumber = document.querySelector('.phone')
+        const percentage = document.querySelector('#percentage')
+        name.textContent = obj.name
+        img.src = obj.image
+        address.innerText = obj.address
+        phoneNumber.innerText = obj.phoneNumber
+        let percentageNumber = obj.likesPercentage
+        percentage.innerText = Math.floor(parseFloat(percentageNumber))+"%"
+
+        console.log("click")
+
+
+    })
 }
+function totalLikes(obj){
+    obj.total = obj.likes + obj.dislikes
+    obj.likesPercentage = obj.likes*100/obj.total 
+    const percentage = document.querySelector("#percentage")
+    percentage.innerText = obj.likesPercentage
+    return obj.likesPercentage
+    
+}
+
 
 
 resturantData();
