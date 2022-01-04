f0d3f892d3d68593cdc77a8974264ff974016704
Gehrig Barnes <gehrigbarnes@gmail.com>
2022-01-04 09:08:58 -0600

    set up the foundation for the HTML. I also added a background color. And I instereted two logos at the top.

---
+++ script.js
@@ -0,0 +1,14 @@
+function resturantData () {
+    fetch("http://localhost:3000/resturants")
+    .then (res => res.json())
+    .then (data => data.forEach(renderSteaks))
+}
+
+function renderSteaks (steaks) {
+    const resturants = document.querySelector("#logo")
+    const logoImage = document.createElement('img')
+    logoImage.setAttribute('src', `${steaks.logo}`)
+    resturants.appendChild(logoImage)
+}
+
+resturantData();
